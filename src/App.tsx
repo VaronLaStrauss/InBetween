0887074664c4431cabdfbf628a1df34d766d8ebf
slrserafin <60497275+slrserafin@users.noreply.github.com>
2022-03-23 01:47:08 +0800

    Sketchy Site Design

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { CardDeck } from "./types/card-deck";
 import CARD_LIST from "./utils/card-list";
 import getCardFromDeck from "./utils/get-deck";
 
+import Button from '@mui/material/Button';
+import Router from 'react-router';
+
 class App extends Component {
   availableCards = new Map([...CARD_LIST]);
   state = {
@@ -17,9 +20,81 @@ class App extends Component {
 
   render(): ReactNode {
     const startButton = (
-      <button className="btn btn-primary" onClick={this.startGame.bind(this)}>
-        Start Game
-      </button>
+      <div className="containerMainMenu">
+        <div className="contentMainMenu">
+          <h1 className="gameTitle">
+            In Between by Wall•E Bayola
+          </h1>
+          <div className="containerChoices">
+            <button className="btn btn-primary" onClick={this.startGame.bind(this)}>
+              Start Game
+            </button>
+            <button className="btn btn-primary">
+              How to play?
+            </button>
+            {/* <Button href="./pages/howtoplay" variant="contained">
+              How to play?
+            </Button> */}
+          </div>
+
+          <div className="htpMainMenu">
+            <table>
+              <tr>
+                <h4>
+                  Developers
+                </h4>
+              </tr>
+              <tr>
+                <td className="viany">
+                </td>
+                <td className="pogs">
+                </td>
+              </tr>
+              <tr>
+                <th>Lead Developer: Viany Manuel</th>
+                <th></th>
+                <th>Developer: Sean Serafin</th>
+              </tr>
+              <tr>
+                <th>Rules:</th>
+              </tr>
+              <tr>
+                <td>I.  There are five rounds every game.</td>
+              </tr>
+              <tr>
+                <td>II. Every round, the game will randomly generate two cards (from Ace to Kings including Jacks)</td>
+              </tr>
+              <tr>
+                <td>III.  The player must choose one of the two options (“Deal” or “No Deal) after the two cards are revealed.</td>
+              </tr>
+              <tr>
+                <td>IV.  The third card will only be shown if the player has selected one of the two options.</td>
+              </tr>
+              <tr>
+                <td>V.  If the player wins the round, a point will be added to the total score.</td>
+              </tr>
+              <tr>
+                <td>VI.  If the player loses the round, a point will be deducted from the total score.</td>
+              </tr>
+              <tr>
+                <td>VII.  If the player chose “No Deal”, half a pont will be deducted from the total score.</td>
+              </tr>
+              <tr>
+                <td>IX.  If the two randomized numbers are identical, the player has the option to choose between HIGHER or LOWER.</td>
+              </tr>
+              <tr>
+                <td>X.  If the user chose HIGHER - the player  WINS the game if the THIRD number is higher than the first two identical drawn numbers. Otherwise, the player LOSES.</td>
+              </tr>
+              <tr>
+                <td>XI.  If the user chose LOWER- the player WINS the game if the THIRD number is higher than the first two identical drawn cards. Otherwise, the player LOSES.</td>
+              </tr>
+              <tr>
+                <td>XII.  If the third randomized number is the same as the first two numbers, it is considered as a loss.</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
     );
     if (this.state.round === 0) {
       return startButton;
@@ -38,16 +113,23 @@ class App extends Component {
 
     return (
       <>
-        <div>
-          <h1>Score: {this.state.score}</h1>
-          <h2>Round: {this.state.round}</h2>
+        <div className="inGameBackGround">
+
+          <div className="statusContainer">
+            <div className="inGameStatus">
+              <h1>Score: {this.state.score}</h1>
+              <h2>Round: {this.state.round}</h2>
+            </div>
+          </div>
+
+          <div className="inGameComponents">
+            <CardDeckComponent {...this.state.card1} />
+            <CardDeckComponent {...this.state.card2} />
+            {!!this.state.card3 && <CardDeckComponent {...this.state.card3} />}
+          </div>
+
+          <div className="inGameActionBtns">{this.getActionButtons(this.state.card1, this.state.card2)}</div>
         </div>
-        <div>
-          <CardDeckComponent {...this.state.card1} />
-          <CardDeckComponent {...this.state.card2} />
-          {!!this.state.card3 && <CardDeckComponent {...this.state.card3} />}
-        </div>
-        <div>{this.getActionButtons(this.state.card1, this.state.card2)}</div>
       </>
     );
   }
