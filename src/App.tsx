4326357470c322cb2787aa5435f5b7b3c7eef32a
Viany Manuel <jeangmanuel@gmail.com>
2022-03-24 19:26:23 +0800

    Finished new game logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,108 +1,148 @@
 import { Component, ReactNode } from "react";
-import "./App.css";
+import "./App.scss";
+import bg from "./assets/background/table-carpet.png";
 import CardDeckComponent from "./card-deck/feature/CardDeck";
+import Instructions from "./instructions/feature/Instructions";
+import SelectedCards from "./selected-cards/feature/SelectedCards";
 import { CardDeck } from "./types/card-deck";
-import CARD_LIST from "./utils/card-list";
-import getCardFromDeck from "./utils/get-deck";
+import { getNewDeck, shuffleDeck } from "./utils/get-deck";
 
-class App extends Component {
-  availableCards = new Map([...CARD_LIST]);
+export default class App extends Component {
+  shuffledDeck!: CardDeck[];
+  usedCards!: Set<number>;
+  unusedCards!: Set<number>;
+
   state = {
-    score: 0,
     round: 0,
-    card3: null,
-    card1: this.playCard,
-    card2: this.playCard,
+    score: 0,
+    selectedCards: [] as CardDeck[],
+    maxRounds: 0,
+    endGame: false,
   };
 
+  constructor(props: any) {
+    super(props);
+    this.resetCounters();
+  }
+
   render(): ReactNode {
-    const startButton = (
-      <button className="btn btn-primary" onClick={this.startGame.bind(this)}>
-        Start Game
-      </button>
-    );
-    if (this.state.round === 0) {
-      return startButton;
-    } else if (this.state.round === 6) {
+    if (this.state.maxRounds > 0) {
+      let turnable = false;
+      return (
+        <div
+          style={{
+            backgroundImage: `url(${bg})`,
+          }}
+          className="App"
+        >
+          <div className="cards-container">
+            {this.shuffledDeck.map((cardDeck, i) => {
+              const key = `field-card-${i}`;
+
+              return (
+                <CardDeckComponent
+                  key={key}
+                  {...cardDeck}
+                  turnable={turnable}
+                  clickCard={this.clickCard.bind(this, cardDeck, i)}
+                  selected={this.usedCards.has(i)}
+                />
+              );
+            })}
+          </div>
+          <SelectedCards
+            full={this.state.selectedCards.length > 1}
+            cards={this.state.selectedCards}
+            handleChoice={this.handleChoice.bind(this)}
+            round={this.state.round}
+            nextRound={this.nextRound.bind(this)}
+          />
+        </div>
+      );
+    }
+
+    if (this.state.endGame) {
       return (
         <div className="App">
-          <h1>
-            Game Over!
-            <br />
-            Score: {this.state.score}
-          </h1>
-          {startButton}
+          <div className="end-game">
+            <h1>Game Over</h1>
+            <h2>Your score: {this.state.score}</h2>
+            <button
+              className="btn btn-primary"
+              onClick={this.startGame.bind(this, 0)}
+            >
+              Restart
+            </button>
+          </div>
         </div>
       );
     }
 
     return (
       <>
-        <div>
-          <h1>Score: {this.state.score}</h1>
-          <h2>Round: {this.state.round}</h2>
-        </div>
-        <div>
-          <CardDeckComponent {...this.state.card1} />
-          <CardDeckComponent {...this.state.card2} />
-          {!!this.state.card3 && <CardDeckComponent {...this.state.card3} />}
-        </div>
-        <div>{this.getActionButtons(this.state.card1, this.state.card2)}</div>
+        <Instructions start={this.startGame.bind(this)} />
       </>
     );
   }
 
-  startGame() {
-    this.availableCards = new Map([...CARD_LIST]);
+  startGame(rounds: number) {
+    this.resetCounters();
     this.setState({
+      round: 0,
       score: 0,
-      round: 1,
+      selectedCards: [] as CardDeck[],
+      endGame: false,
+      maxRounds: rounds,
     });
+  }
+
+  resetCounters() {
+    this.shuffledDeck = shuffleDeck(getNewDeck());
+    this.usedCards = new Set<number>();
+    this.unusedCards = new Set<number>(this.shuffledDeck.map((_, i) => i));
   }
 
   nextRound() {
+    if (this.state.maxRounds - 1 === this.state.round) {
+      return this.setState({
+        ...this.state,
+        round: 0,
+        selectedCards: [] as CardDeck[],
+        maxRounds: 0,
+        endGame: true,
+      });
+    }
+
     this.setState({
       ...this.state,
       round: this.state.round + 1,
-      card3: null,
-      card1: this.playCard,
-      card2: this.playCard,
+      selectedCards: [] as CardDeck[],
     });
   }
 
-  getActionButtons(card1: CardDeck, card2: CardDeck) {
-    if (!!this.state.card3) {
-      return (
-        <button className="btn btn-primary" onClick={this.nextRound.bind(this)}>
-          {this.state.round === 5 ? "End Game" : "Next Round"}
-        </button>
-      );
-    }
+  clickCard(card: CardDeck, i: number) {
+    if (this.usedCards.has(i) || this.state.selectedCards.length >= 2) return;
+    this.usedCards.add(i);
+    this.unusedCards.delete(i);
 
-    const choices = ["No Deal"];
-    card1.cardNumber === card2.cardNumber
-      ? choices.push("Higher", "Lower")
-      : choices.push("Deal");
-
-    return choices.map((choice, i) => {
-      const key = `${choice}-${i}`;
-      return (
-        <button
-          className="btn btn-primary"
-          onClick={this.handleChoice.bind(this, choice, card1, card2)}
-          key={key}
-        >
-          {choice}
-        </button>
-      );
+    this.setState({
+      ...this.state,
+      selectedCards: [...this.state.selectedCards, card],
     });
   }
 
-  handleChoice(choice: string, card1: CardDeck, card2: CardDeck) {
+  handleChoice(choice: string) {
+    const [card1, card2] = this.state.selectedCards;
     const card3 = this.playCard;
-    const state = { ...this.state, card3 };
+
+    const cards = [card1, card2, card3];
+
     if (choice === "No Deal") {
-      return this.setState({ ...state, score: this.state.score - 0.5 });
+      return this.setState({
+        ...this.state,
+        score: this.state.score - 0.5,
+        selectedCards: cards,
+      });
     }
 
     if (card1.cardNumber === card2.cardNumber) {
@@ -110,34 +150,43 @@ class App extends Component {
         (card3.cardNumber > card1.cardNumber && choice === "Higher") ||
         (card3.cardNumber < card1.cardNumber && choice === "Lower")
       ) {
-        return this.setState({ ...state, score: this.state.score + 1 });
+        return this.setState({
+          ...this.state,
+          score: this.state.score + 1,
+          selectedCards: cards,
+        });
       } else {
-        return this.setState({ ...state, score: this.state.score - 1 });
+        return this.setState({
+          ...this.state,
+          score: this.state.score - 1,
+          selectedCards: cards,
+        });
       }
     }
 
     const min = Math.min(card1.cardNumber, card2.cardNumber);
     const max = Math.max(card1.cardNumber, card2.cardNumber);
     if (card3.cardNumber > min && card3.cardNumber < max && choice === "Deal") {
-      return this.setState({ ...state, score: this.state.score + 1 });
+      return this.setState({
+        ...this.state,
+        score: this.state.score + 1,
+        selectedCards: cards,
+      });
     } else {
-      return this.setState({ ...state, score: this.state.score - 1 });
+      return this.setState({
+        ...this.state,
+        score: this.state.score - 1,
+        selectedCards: cards,
+      });
     }
   }
 
   get playCard(): CardDeck {
-    const card = getCardFromDeck(this.availableCards);
-    const list = this.availableCards.get(card.suit)!.filter((val) => {
-      return val !== card.cardNumber;
-    });
-    if (list.length === 0) {
-      this.availableCards.delete(card.suit);
-    } else {
-      this.availableCards.set(card.suit, list);
-    }
-
-    return card;
+    const i = Array.from(this.unusedCards)[
+      Math.floor(Math.random() * this.unusedCards.size)
+    ];
+    this.usedCards.add(i);
+    this.unusedCards.delete(i);
+    return this.shuffledDeck[i];
   }
 }
-
-export default App;
--- src/utils/get-deck.ts
+++ src/utils/get-deck.ts
@@ -1,7 +1,8 @@
 import { CardDeck } from "../types/card-deck";
+import SUITE_LIST from "./suite-list";
 import { SUIT_LOCATIONS } from "./suite-location";
 
-export default function getCardFromDeck(deck: Map<string, number[]>): CardDeck {
+export function getCardFromDeck(deck: Map<string, number[]>): CardDeck {
   const keys = Array.from(deck.keys());
   const randomVal = Math.floor(Math.random() * keys.length);
   const suit = keys[randomVal];
@@ -16,4 +17,38 @@ export default function getCardFromDeck(deck: Map<string, number[]>): CardDeck {
     cardNumber,
     locations,
   };
+}
+
+export function shuffleDeck(deck: Map<string, number[]>): CardDeck[] {
+  const copiedDeck = copyDeck(deck);
+
+  const cards: CardDeck[] = [];
+  while (copiedDeck.size > 0) {
+    const card = getCardFromDeck(copiedDeck);
+    cards.push(card);
+    copiedDeck
+      .get(card.suit)!
+      .splice(copiedDeck.get(card.suit)!.indexOf(card.cardNumber), 1);
+    if (copiedDeck.get(card.suit)!.length === 0) {
+      copiedDeck.delete(card.suit);
+    }
+  }
+
+  return cards;
+}
+
+export function getNewDeck(): Map<string, number[]> {
+  return new Map(
+    SUITE_LIST.map((suite) => {
+      return [suite, Array.from({ length: 13 }, (_, i) => i + 1)];
+    })
+  );
+}
+
+export function copyDeck(deck: Map<string, number[]>) {
+  const newDeck = new Map<string, number[]>();
+  for (const [suit, numbers] of deck) {
+    newDeck.set(suit, [...numbers]);
+  }
+  return newDeck;
 }
